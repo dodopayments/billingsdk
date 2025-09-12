2b14cac4a1874a8049451817a63cf090388ab6ad
Megh Bari <meghbari01@gmail.com>
2025-09-13 01:43:09 +0530

    feat(ui): add loading screen to improve initial page load UX

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,10 +1,10 @@
 import type { ReactNode } from "react";
-import NavBar from "@/components/landing/NavBar";
+// import NavBar from "@/components/landing/NavBar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="w-full max-w-[1920px] mx-auto min-h-screen bg-background">
-      <NavBar />
+      {/* <NavBar /> */}
       {children}
     </div>
   );
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,34 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Footer2 } from "@/components/landing/footer2";
 import Hero from "@/components/landing/Hero";
 import { ComponentsSection } from "@/components/landing/components";
 import Features from "@/components/landing/Features";
 import { Footer } from "@/components/landing/footer";
 import { OpenSource } from "@/components/landing/opensource";
+import { LoadingScreen } from "@/components/landing/loading-screen";
+import NavBar from "@/components/landing/NavBar";
 
 const Page = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2500); // 2.5 seconds - as per i calculated
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  
+  if (isLoading) {
+    return <LoadingScreen onLoadingComplete={() => setIsLoading(false)} />;
+  }
+
   return (
     <main className="w-full relative overflow-hidden p-4">
+      <NavBar />
       <Hero />
       <Features />
       <ComponentsSection />
