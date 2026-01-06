0a26bbd297df6e502f6c07e6da0ee01c28a19ac2
HarshVz <harshdarji.dev@gmail.com>
2026-01-07 00:56:21 +0530

    fix: promise

--- src/components/payment-processing-demo.tsx
+++ src/components/payment-processing-demo.tsx
@@ -2,17 +2,18 @@
 
 import { useState } from "react";
 import PaymentProcessing from "@/registry/billingsdk/payment-processing";
-import { setTimeout } from "timers";
 import { Button } from "@/components/ui/button";
 
 export function PaymentProcessingDemo() {
     const [loading, setLoading] = useState(false);
     const handleProcessing = async () => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 8000);
+        return new Promise<void>((resolve) => {
+            setTimeout(() => {
+                setLoading(false);
+                resolve();
+            }, 8000);
+        });
     };
-
     const onClickHandler = async () => {
         setLoading(true);
         await handleProcessing();
